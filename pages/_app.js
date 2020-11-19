be943ed75e37007d7a4b811d63d09230dbc51538
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-11-19 06:35:09 -0600

    cleaned up code, passing single client around instead of building one per page. Also set up auth for chlid pages

--- pages/_app.js
+++ pages/_app.js
@@ -7,19 +7,21 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <PageHeader appName={appName} currentUser={currentUser} />
-      <Component appName={appName} {...pageProps} />
+      {/* pass auth validation to components */}
+      <Component appName={appName} currentUser={currentUser} {...pageProps} />
     </div>
   );
 };
 
 AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
+  // check for auth globally
   const { data } = await client.get('/api/users/currentuser');
 
   let pageProps = {};
 
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data.currentUser);
   }
 
   return {
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
-import buildClient from '../api/build-client';
 import PageContainer from '../components/page-container';
 
 const LandingPage = ({ appName, currentUser }) => {
   return <PageContainer pageName={`Welcome to ${appName}`} />;
 };
 
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default LandingPage;
