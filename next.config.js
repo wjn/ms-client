837da75da6de44fd32dd97da3b422420c7ce5a94
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-11-24 13:27:14 -0600

    fixed the webpack bullshit preventing pages from compiling

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ module.exports = {
     config.watchOptions.poll = 300;
     return config;
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // These components were keeping things from building. This insures that
+      // they are only called when on the server (instead of the browser)
+      config.node = {
+        net: 'empty',
+        fs: 'empty',
+        tls: 'empty',
+      };
+    }
+
+    return config;
+  },
 };
